660a9b5312cc72b98111038a3deedb88b33b2937
TimDM1980 <timdemeyer1980@gmail.com>
2023-10-29 15:14:32 +0100

    move business logic out of grid

--- src/agile-practices-distilled/tictactoe/v2/grid.ts
+++ src/agile-practices-distilled/tictactoe/v2/grid.ts
@@ -1,9 +1,8 @@
-import { Mark, modChecks, Move } from './types';
+import { Mark, Move } from './types';
 import { onGrid, Position } from './positions';
 
 const TOTAL_POSITIONS_ON_GRID = 9;
 
-// TODO OBJECT CALISTHENICS - keep all entities small - 50 lines per class
 export class Grid {
   private readonly grid: Mark[][];
 
@@ -32,41 +31,12 @@ export class Grid {
     return this.countNumberOfMarksInGrid() === TOTAL_POSITIONS_ON_GRID;
   }
 
-  makeMove(move: Move): void {
-    this.validateMove(move);
-    this.markGrid(move);
-  }
-
-  private validateMove(move: Move) {
-    this.validateXStartsTheGame(move);
-    this.validateAlternateTurns(move);
-    this.validateMarkFreePosition(move);
-  }
-
-  private validateXStartsTheGame(move: Move) {
-    if (move.mark !== Mark.X && this.countNumberOfMarksInGrid() === 0) {
-      throw Error('X always goes first!');
-    }
-  }
-
-  private validateAlternateTurns(move: Move) {
-    if (!(this.countNumberOfMarksInGrid() % 2 === modChecks.get(move.mark)!)) {
-      throw Error('Players alternate placing X and O on the board!');
-    }
-  }
-
-  private validateMarkFreePosition(move: Move) {
-    if (this.hasMark(move.position)) {
-      throw Error('Players cannot play on a played position!');
-    }
-  }
-
-  private markGrid(move: Move) {
-    this.grid[onGrid(move.position).x][onGrid(move.position).y] = move.mark;
-  }
-
   // TODO OBJECT CALISTHENICS - wrap primitives
-  private countNumberOfMarksInGrid() {
+  countNumberOfMarksInGrid() {
     return this.grid.reduce((acc, currentRow) => acc + currentRow.reduce((acc) => acc + 1, 0), 0);
+  }
+
+  markGrid(move: Move) {
+    this.grid[onGrid(move.position).x][onGrid(move.position).y] = move.mark;
   }
 }
--- src/agile-practices-distilled/tictactoe/v2/tictactoe.ts
+++ src/agile-practices-distilled/tictactoe/v2/tictactoe.ts
@@ -1,8 +1,9 @@
-import { Mark, markOutcomes, Outcome } from './types';
+import { Mark, markOutcomes, modChecks, Move, Outcome } from './types';
 import { Position } from './positions';
 import { Grid } from './grid';
 import { OutcomeService } from './outcome-service';
 
+// TODO OBJECT CALISTHENICS - keep all entities small - 50 lines per class
 export class TicTacToe {
   // TODO OBJECT CALISTHENICS - no classes with more than 2 instance variables
   private grid: Grid;
@@ -17,13 +18,38 @@ export class TicTacToe {
 
   public makeMove(mark: Mark, position: Position): void {
     this.validateGameNotYetDecided(this.outcome);
-    this.grid.makeMove({ mark, position });
+    this.validateMove({ mark, position });
+    this.grid.markGrid({ mark, position });
     this.outcome = this.outcomeService.determineOutcome(this.grid);
   }
 
   private validateGameNotYetDecided(outcome: Outcome) {
     if (outcome !== Outcome.NOT_YET_DECIDED) {
       throw Error('Cant make a move, game is already over!');
+    }
+  }
+
+  private validateMove(move: Move) {
+    this.validateXStartsTheGame(move);
+    this.validateAlternateTurns(move);
+    this.validateMarkFreePosition(move);
+  }
+
+  private validateXStartsTheGame(move: Move) {
+    if (move.mark !== Mark.X && this.grid.countNumberOfMarksInGrid() === 0) {
+      throw Error('X always goes first!');
+    }
+  }
+
+  private validateAlternateTurns(move: Move) {
+    if (!(this.grid.countNumberOfMarksInGrid() % 2 === modChecks.get(move.mark)!)) {
+      throw Error('Players alternate placing X and O on the board!');
+    }
+  }
+
+  private validateMarkFreePosition(move: Move) {
+    if (this.grid.hasMark(move.position)) {
+      throw Error('Players cannot play on a played position!');
     }
   }
 
