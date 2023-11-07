9dc9f9513211ff98d32138e533623faa1932a5b3
TimDM1980 <timdemeyer1980@gmail.com>
2023-11-07 23:12:25 +0100

    refactorings

--- src/agile-practices-distilled/codesmells/game.ts
+++ src/agile-practices-distilled/codesmells/game.ts
@@ -1,65 +1,84 @@
 export class Game {
-  private _lastSymbol: string = ' ';
-  private _board: Board = new Board();
+  private lastSymbol: Symbol = ' ';
+  private board: Board = new Board();
 
-  public Play(symbol: string, x: number, y: number) : void {
-    //if first move
-    if (this._lastSymbol == ' ') {
-      //if player is X
-      if (symbol == 'O') {
-        throw new Error("Invalid first player");
-      }
+  public play(symbol: Symbol, x: number, y: number): void {
+    if (this.isFirstPlayerInvalid(symbol)) {
+      throw new Error('Invalid first player');
     }
-    //if not first move but player repeated
-    else if (symbol == this._lastSymbol) {
-      throw new Error("Invalid next player");
+    if (this.isPlayerRepeated(symbol)) {
+      throw new Error('Invalid next player');
     }
-    //if not first move but play on an already played tile
-    else if (this._board.TileAt(x, y).Symbol != ' ') {
-      throw new Error("Invalid position");
+    if (this.board.isTilePlayed(x, y)) {
+      throw new Error('Invalid position');
     }
 
-    // update game state
-    this._lastSymbol = symbol;
-    this._board.AddTileAt(symbol, x, y);
+    this.lastSymbol = symbol;
+    this.board.addTileAt(symbol, x, y);
   }
 
-  public Winner() : string {
-    //if the positions in first row are taken
-    if (this._board.TileAt(0, 0)!.Symbol != ' ' &&
-      this._board.TileAt(0, 1)!.Symbol != ' ' &&
-      this._board.TileAt(0, 2)!.Symbol != ' ') {
-      //if first row is full with same symbol
-      if (this._board.TileAt(0, 0)!.Symbol ==
-        this._board.TileAt(0, 1)!.Symbol &&
-        this._board.TileAt(0, 2)!.Symbol == this._board.TileAt(0, 1)!.Symbol) {
-        return this._board.TileAt(0, 0)!.Symbol;
-      }
-    }
+  private isFirstPlayerInvalid(symbol: Symbol) {
+    return this.lastSymbol === ' ' && symbol !== 'X';
+  }
 
-    //if the positions in first row are taken
-    if (this._board.TileAt(1, 0)!.Symbol != ' ' &&
-      this._board.TileAt(1, 1)!.Symbol != ' ' &&
-      this._board.TileAt(1, 2)!.Symbol != ' ') {
-      //if middle row is full with same symbol
-      if (this._board.TileAt(1, 0)!.Symbol ==
-        this._board.TileAt(1, 1)!.Symbol &&
-        this._board.TileAt(1, 2)!.Symbol ==
-        this._board.TileAt(1, 1)!.Symbol) {
-        return this._board.TileAt(1, 0)!.Symbol;
-      }
-    }
+  private isPlayerRepeated(symbol: Symbol) {
+    return symbol === this.lastSymbol;
+  }
 
-    //if the positions in first row are taken
-    if (this._board.TileAt(2, 0)!.Symbol != ' ' &&
-      this._board.TileAt(2, 1)!.Symbol != ' ' &&
-      this._board.TileAt(2, 2)!.Symbol != ' ') {
-      //if middle row is full with same symbol
-      if (this._board.TileAt(2, 0)!.Symbol ==
-        this._board.TileAt(2, 1)!.Symbol &&
-        this._board.TileAt(2, 2)!.Symbol ==
-        this._board.TileAt(2, 1)!.Symbol) {
-        return this._board.TileAt(2, 0)!.Symbol;
+  public winner(): Symbol {
+    const rowsToCheck = [
+      this.symbolForRow([
+        { x: 0, y: 0 },
+        { x: 0, y: 1 },
+        { x: 0, y: 2 },
+      ]),
+      this.symbolForRow([
+        { x: 1, y: 0 },
+        { x: 1, y: 1 },
+        { x: 1, y: 2 },
+      ]),
+      this.symbolForRow([
+        { x: 2, y: 0 },
+        { x: 2, y: 1 },
+        { x: 2, y: 2 },
+      ]),
+
+      this.symbolForRow([
+        { x: 0, y: 0 },
+        { x: 1, y: 0 },
+        { x: 2, y: 0 },
+      ]),
+      this.symbolForRow([
+        { x: 0, y: 1 },
+        { x: 1, y: 1 },
+        { x: 2, y: 1 },
+      ]),
+      this.symbolForRow([
+        { x: 0, y: 2 },
+        { x: 1, y: 2 },
+        { x: 2, y: 2 },
+      ]),
+
+      this.symbolForRow([
+        { x: 0, y: 0 },
+        { x: 1, y: 1 },
+        { x: 2, y: 2 },
+      ]),
+      this.symbolForRow([
+        { x: 0, y: 2 },
+        { x: 1, y: 1 },
+        { x: 2, y: 0 },
+      ]),
+    ];
+
+    return rowsToCheck.find((symbol) => symbol !== ' ') ?? ' ';
+  }
+
+  public symbolForRow(positions: { x: number; y: number }[]): Symbol {
+    if (positions.every((position) => this.board.isTilePlayed(position.x, position.y))) {
+      const symbolToCheck = this.board.symbolAt(positions[0].x, positions[0].y);
+      if (positions.every((position) => this.board.symbolAt(position.x, position.y) === symbolToCheck)) {
+        return symbolToCheck;
       }
     }
 
@@ -67,37 +86,38 @@ export class Game {
   }
 }
 
-interface Tile
-{
-  X: number;
-  Y: number;
-  Symbol: string;
+type Symbol = 'X' | 'O' | ' ';
+
+interface Tile {
+  x: number;
+  y: number;
+  symbol: Symbol;
 }
 
-class Board
-{
-  private _plays : Tile[] = [];
+class Board {
+  private tiles: Tile[] = [];
 
-  constructor()
-  {
-    for (let i = 0; i < 3; i++)
-    {
-      for (let j = 0; j < 3; j++)
-      {
-        const tile : Tile = {X :i, Y:j, Symbol:" "};
-        this._plays.push(tile);
+  constructor() {
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        this.tiles.push({ x: i, y: j, symbol: ' ' });
       }
     }
   }
 
-  public TileAt(x:  number, y: number): Tile {
-    return this._plays.find((t:Tile) => t.X == x && t.Y == y)!
+  isTilePlayed(x: number, y: number): boolean {
+    return this.symbolAt(x, y) !== ' ';
   }
 
-  public AddTileAt(symbol: string, x: number, y: number) : void
-  {
-    const tile : Tile = {X :x, Y:y, Symbol:symbol};
+  symbolAt(x: number, y: number): Symbol {
+    return this.tileAt(x, y).symbol;
+  }
 
-    this._plays.find((t:Tile) => t.X == x && t.Y == y)!.Symbol = symbol;
+  addTileAt(symbol: Symbol, x: number, y: number): void {
+    this.tileAt(x, y).symbol = symbol;
+  }
+
+  private tileAt(x: number, y: number): Tile {
+    return this.tiles.find((t: Tile) => t.x === x && t.y === y)!;
   }
 }
